ec7fb192cf7b7c00c33187c3a4da15f4ea302e98
JILSE7 <saidnnnn@gmail.com>
2023-04-20 18:29:25 -0600

    agregando readme

--- back/dist/router/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var controller_1 = require("../controller");
-var router = (0, express_1.Router)();
-router.get('/best-in-town', controller_1.RestaurantController.SearchRestaurantController);
-exports.default = router;
